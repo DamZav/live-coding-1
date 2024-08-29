5267277a1960dddbfad99eadc686da579c3d5632
DamZav <damaris.zavala91@gmail.com>
2024-08-29 17:32:56 -0600

    faltan funciones

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // Escribe tu código aquí.
+
+function bigWords(inputString, stringArray) {
+    return stringArray.filter(word => word.length > inputString.length);
+}
+
+const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+console.log(bigWords('bocina', myArray));
+
+
+
+function printArray(array) {
+    return
+}
+
+const myArray2 = ['insecto', 'bootcamp', 'escritorio'];
+printArray(myArray2);
